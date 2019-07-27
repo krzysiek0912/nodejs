3823cf95ef012cd620430046aaefa940abb9e134
Krzysztof Branecki <krzysztof@branecki.net>
2019-07-27 16:12:30 +0200

    Edit function formatData

--- module/formatData.js
+++ module/formatData.js
@@ -1,22 +1,10 @@
 function fotmatData(data) {
-  let output = "",
-    oneHour = 60 * 60,
-    oneMiute = 60,
-    hours = 0,
-    minuts = 0;
+  let sec_num = parseInt(data),
+    hours = Math.floor(sec_num / 3600),
+    minutes = Math.floor((sec_num - hours * 3600) / 60),
+    seconds = sec_num - hours * 3600 - minutes * 60;
 
-  if (data > oneHour) {
-    hours = Math.floor(data / oneHour);
-  }
-  if (data > oneMiute) {
-    minuts = Math.floor((data - hours * oneHour) / oneMiute);
-  }
-  seconds = data - hours * oneHour - minuts * oneMiute;
-
-  hours > 0 ? (hours = hours + " godz. ") : (hours = "");
-  minuts > 0 ? (minuts = minuts + " min. ") : (minuts = "");
-
-  return hours + minuts + seconds + " sek.";
+  return hours + " godz. " + minutes + " min. " + seconds + " sec.";
 }
 
 exports.print = fotmatData;
